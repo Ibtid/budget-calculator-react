533fcd2434adae019a9f418a39c3ac5f51b33a75
Ibtid <ibtid011@example.com>
2020-06-02 00:52:31 +0600

    comit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import ExpenseList from "./components/ExpenseList";
+import ExpenseForm from "./components/ExpenseForm";
+import Alert from "./components/Alert";
+import { v4 as uuid } from "uuid";
+
+const initialExpenses = [
+  { id: uuid(), charge: "rent", amount: 190 },
+  { id: uuid(), charge: "salary", amount: 192 },
+];
 
 function App() {
+  const [expenses, setExpenses] = useState(initialExpenses);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Alert />
+      <h1>budget calculator</h1>
+      <main className="App">
+        <ExpenseForm />
+        <ExpenseList expenses={expenses} />
+      </main>
+      <h1>
+        total spending : {""}
+        <span className="total">
+          $
+          {expenses.reduce((accumulator, current) => {
+            return (accumulator += current.amount);
+          }, 0)}
+        </span>
+      </h1>
+    </>
   );
 }
 
