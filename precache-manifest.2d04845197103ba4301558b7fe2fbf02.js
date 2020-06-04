7c598c09a5cfc116616af5206b9830c674acb1d1
Ibtid <ibtid011@example.com>
2020-06-05 00:38:33 +0600

    Updates

---
+++ precache-manifest.2d04845197103ba4301558b7fe2fbf02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40318ec5746aa914b0072e3e983372cf",
+    "url": "/budget-calculator-react/index.html"
+  },
+  {
+    "revision": "5c206930e287e69c62a2",
+    "url": "/budget-calculator-react/static/css/main.aca86320.chunk.css"
+  },
+  {
+    "revision": "2178015a8cbce19f168b",
+    "url": "/budget-calculator-react/static/js/2.a63c9706.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/budget-calculator-react/static/js/2.a63c9706.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c206930e287e69c62a2",
+    "url": "/budget-calculator-react/static/js/main.c5628b78.chunk.js"
+  },
+  {
+    "revision": "f581e5dad1d1c8d7f21f",
+    "url": "/budget-calculator-react/static/js/runtime-main.1ad14855.js"
+  }
+]);
